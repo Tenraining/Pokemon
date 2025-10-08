8f62d14029c8ea25a34220f1e3cba37b9a9a0ffb
Tenraining <166075505+Tenraining@users.noreply.github.com>
2025-10-07 19:04:49 -0600

    Added stuff

--- Pokemon/Pokemon.cs
+++ Pokemon/Pokemon.cs
@@ -1,8 +1,12 @@
-﻿namespace Pokemon
+﻿using System.Net;
+
+namespace Pokemon
 {
     public class Pokemon
     {
-       
+       //Pokemon will be the parent class
+
+
         //data members
 
         private string _Name;
@@ -15,18 +19,33 @@
         private const int MINIMUMLEVEL = 1;
         private const int MAXIMUMLEVEL = 100;
 
+
+        
+
+
         public string Name { get; set; }
         public string Type { get; set; }
         public int Level { get; set; }
 
         public Pokemon(string name, string type, int level)
         {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("LastName", "Last name cannot be missing or blank");
+            }
             Name = name;
             Type = type;
             Level = level;
-        }
 
-        
+            if (employments != null)
+            {
+                EmploymentPositions = employments; //save it
+            }
+        }
+        //The parent class is the one where this property is declared.
+        public List<BaseStats> Pokemonsds { get; private set; } = new List<BaseStats>();
+
+
     }
 
 }
---
+++ Pokemon_Should/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Pokemon_Should
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void pokemon_should_have_a_name()
+        {
+
+        }
+    }
+}
